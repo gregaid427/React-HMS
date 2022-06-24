07c6452f3504febcb57711a18944a99bec27d116
Gregory Aidoo <gregory.aidoo@amalitech.org>
2022-06-24 09:45:11 +0000

    initial setup

---
+++ client/hms/src/pages/lab.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from 'style.scss';
+import {CssBaseline} from '@mui/material';
+
+export const lab = () => {
+    return (<>
+        <CssBaseline/>
+      
+        
+        </>
+        )
+      }
+
+export default lab
---
+++ client/hms/src/pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from 'style.scss';
+import {CssBaseline} from '@mui/material';
+
+const login = () => {
+  return (<>
+  <CssBaseline/>
+
+
+  </>
+  )
+}
+
+export default login

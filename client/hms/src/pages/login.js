7f02ef4f76d20390dbeaaa1b7a52bd64003b6521
Gregory Aidoo <gregory.aidoo@amalitech.org>
2022-06-24 12:47:00 +0000

    fix-errors

--- client/hms/src/pages/login.js
+++ client/hms/src/pages/login.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import style from 'style.scss';
+import style from "../style.scss";
 import {CssBaseline} from '@mui/material';
 
 const login = () => {
   return (<>
   <CssBaseline/>
+dsfhkdbfhfb
 
 
   </>
